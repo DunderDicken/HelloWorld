607331f32a836344e8939bf08cd21be500154a9c
dickk <dickk@AsusLaptop>
2017-10-31 11:00:10 +0100

    First push

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!" + testString());
+	}
+	
+	public static String testString() {
+		return "TEST";
+	}
+}
