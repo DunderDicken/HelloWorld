39075eb5478d40d7446cb872978e95b9416d2450
Dunder <Dunder@PICKADULVER>
2017-11-14 11:07:21 +0100

    Moved files

--- Test_Lectures/src/MyIPAdress.java
+++ Test_Lectures/src/MyIPAdress.java
@@ -1,13 +1,24 @@
+import java.io.IOException;
 import java.net.*;
 public class MyIPAdress {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		try {
+			
+			
 			InetAddress you = InetAddress.getByName("www.oru.se");
+			if(you.isReachable(10)) {
+				System.out.println("REACHABLE!");
+			}
+				
 			System.out.println(you);
+			System.out.println(you.getHostName());
+			System.out.println(you.getHostAddress());
 			
 			InetAddress me = InetAddress.getLocalHost();
 			//String dottedQuad = me.toString();
+//			System.out.println(me.getHostName());
+//			System.out.println(me.getHostAddress());
 			
 			System.out.println("My address is " + me.toString());
 		} catch (UnknownHostException ex) {
--- Test_Lectures/src/test/StaticLambdaOsv.java
+++ Test_Lectures/src/test/StaticLambdaOsv.java
@@ -1,3 +1,4 @@
+package test;
  class Things{
 	 protected String name;
 	 protected static String descritption;
